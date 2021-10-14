fce5df46b2b755128836d2100793958fb3926b21
Akbar Hussain Sabri <akbar.hussain@devsinc.com>
2021-10-14 20:05:16 +0500

    authorizing user to edit profile and post

--- test/policies/user_policy_test.rb
+++ test/policies/user_policy_test.rb
@@ -1,15 +1,18 @@
-# frozen_string_literal: true
-
 require 'test_helper'
 
 class UserPolicyTest < ActiveSupport::TestCase
-  def test_scope; end
+  def test_scope
+  end
 
-  def test_show; end
+  def test_show
+  end
 
-  def test_create; end
+  def test_create
+  end
 
-  def test_update; end
+  def test_update
+  end
 
-  def test_destroy; end
+  def test_destroy
+  end
 end

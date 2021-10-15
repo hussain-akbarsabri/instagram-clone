6f22a510b552839b9980f5ed2c68c6389c9ba301
Akbar Hussain Sabri <akbar.hussain@devsinc.com>
2021-10-16 01:12:02 +0500

    login and signup page UI updated

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     user_name: 'akbar.hussain@devsinc.com',
-    password: 'bqtunomechsazztl',
+    password: 'dajwmbtrurvumkzf',
     authentication: 'plain',
     enable_starttls_auto: true
   }

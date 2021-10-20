92c911905cc1e0bdf4e2acab6f741d1187763cd0
Akbar Hussain Sabri <akbar.hussain@devsinc.com>
2021-10-20 13:33:57 +0500

    Updated UI of feed page

--- config/environment.rb
+++ config/environment.rb
@@ -14,3 +14,12 @@ Cloudinary.config do |config|
   config.enhance_image_tag = true
   config.static_file_support = true
 end
+
+config.action_mailer.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: 587,
+  user_name: ENV['GMAIL_USERNAME'],
+  password: ENV['GMAIL_PASSWORD'],
+  authentication: 'plain',
+  enable_starttls_auto: true
+}

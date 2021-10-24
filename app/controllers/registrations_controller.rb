116690d4c4a1b1f74b2bff40281a6a8106974f2b
Akbar Hussain Sabri <akbar.hussain@devsinc.com>
2021-10-24 18:49:49 +0500

    added br in sign up form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -17,8 +17,4 @@ class RegistrationsController < Devise::RegistrationsController
   def update_resource(resource, params)
     resource.update_without_password(params)
   end
-
-  def after_sign_out_path_for(resource)
-    new_user_session_path(resource)
-  end
 end

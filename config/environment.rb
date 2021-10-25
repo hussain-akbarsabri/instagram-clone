24114c8bf539937d0e97ee4df35261f016d7c2f6
Akbar Hussain Sabri <akbar.hussain@devsinc.com>
2021-10-25 18:09:22 +0500

    gmail setting up

--- config/environment.rb
+++ config/environment.rb
@@ -16,6 +16,9 @@ Cloudinary.config do |config|
 end
 
 ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.default charset: 'utf-8'
 ActionMailer::Base.smtp_settings = {
   address: 'smtp.gmail.com',
   port: 587,

223e1801af66677c8640da07bc0661d2c604cb64
Akbar Hussain Sabri <akbar.hussain@devsinc.com>
2021-10-26 21:20:46 +0500

    seed file creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,29 +8,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(
-  email: 'taha@gmail.com',
-  password: '123123',
-  password_confirmation: '123123',
-  username: 'taha_',
-  name: 'taha',
-  bio: 'i am here'
-)
+require 'open-uri'
+require 'faker'
 
-User.create(
-  email: 'ali@gmail.com',
-  password: '123123',
-  password_confirmation: '123123',
-  username: 'ali_',
-  name: 'ali',
-  bio: 'i am here'
-)
+def image_fetcher
+  URI.parse(Faker::Avatar.image).open
+end
 
-User.create(
-  email: 'ahmad@gmail.com',
-  password: '123123',
-  password_confirmation: '123123',
-  username: 'ahmad_',
-  name: 'ahmad',
-  bio: 'i am here'
-)
+100.times do |n|
+  user = User.create(
+    email: Faker::Internet.email,
+    password: '123123',
+    password_confirmation: '123123',
+    name: Faker::Name.name,
+    username: Faker::Name.name,
+    bio: Faker::Name.name
+  )
+  user.image.attach({
+                      io: image_fetcher,
+                      filename: "#{n}_faker_image.jpg"
+                    })
+end

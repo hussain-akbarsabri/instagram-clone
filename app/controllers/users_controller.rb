e3ea94a89be297ed9301961e808e5de11e64c7c6
Akbar Hussain Sabri <akbar.hussain@devsinc.com>
2021-10-27 15:47:59 +0500

    added html response for autocomplete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,16 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :force_json, only: :search
-
   def search
-    @users = User.ransack(username_cont: params[:q]).result(distinct: true).limit(5)
+    @users = User.ransack(username_cont: params[:q]).result(distinct: true)
+
+    respond_to do |format|
+      format.html {}
+      format.json { @users = @users.limit(10) }
+    end
   end
 
   def show
     @user = User.find(params[:id])
-  end
-
-  private
-
-  def force_json
-    request.format = :json
-  end
-
-  def force_json
-    request.format = :json
   end
 end

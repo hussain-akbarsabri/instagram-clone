701c789d629dff1378f97a9c453d5411bde36eac
Akbar Hussain Sabri <akbar.hussain@devsinc.com>
2021-10-28 23:10:35 +0500

    redis endpoint

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,24 @@
 # frozen_string_literal: true
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'], size: 4, network_timeout: 5 }
+if Rails.env.development?
+  Sidekiq.configure_client do |config|
+    config.redis = { url: 'redis://localhost:6379', size: 4, network_timeout: 5 }
+  end
+
+  Sidekiq.configure_server do |config|
+    config.redis = { url: 'redis://localhost:6379', size: 4, network_timeout: 5 }
+  end
 end
 
-Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'], size: 4, network_timeout: 5 }
+if Rails.env.production?
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV['REDISCLOUD_CRIMSON_URL'], size: 1, network_timeout: 5 }
+  end
+
+  Sidekiq.configure_server do |config|
+    pool_size = (Sidekiq.options[:concurrency] + 2)
+    config.redis = { url: ENV['REDISCLOUD_CRIMSON_URL'], size: pool_size, network_timeout: 5 }
+  end
+
+  Sidekiq::Extensions.enable_delay!
 end

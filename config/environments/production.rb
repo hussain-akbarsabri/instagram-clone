d8bcc03a4ad39f23a0d73568059446c1c721dcbe
Akbar Hussain Sabri <akbar.hussain@devsinc.com>
2021-10-28 19:00:26 +0500

    initailzer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,15 +6,6 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  Cloudinary.config do |config|
-    config.cloud_name = ENV['CLOUDINARY_CLOUD_NAME']
-    config.api_key = ENV['CLOUDINARY_API_KEY']
-    config.api_secret = ENV['CLOUDINARY_API_SECRET']
-    config.secure = true
-    config.enhance_image_tag = true
-    config.static_file_support = true
-  end
-
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.

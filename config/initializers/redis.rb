2dea663bc2ac1a4c9053e4e06aee417c6b80787f
Akbar Hussain Sabri <akbar.hussain@devsinc.com>
2021-10-29 12:28:57 +0500

    redis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-Redis.new(host: ENV['REDIS_END_POINT'], port: ENV['REDIS_PORT'], password: ENV['REDIS_PASSWORD'])
+$redis = Resque.redis = Redis.new(url: ENV['REDIS_END_POINT']) if ENV['REDIS_END_POINT']

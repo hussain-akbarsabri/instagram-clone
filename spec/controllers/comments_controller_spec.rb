e9248c57572ac7db34e224d3d5a57602c49809b4
Akbar Hussain Sabri <akbar.hussain@devsinc.com>
2021-11-07 13:28:40 +0500

    comments_controller_spec authentication

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -4,40 +4,60 @@ require 'rails_helper'
 require_relative '../support/devise'
 
 RSpec.describe CommentsController, type: :controller do
-  let!(:user) { FactoryBot.create(:user) }
-  let!(:comment_post) { FactoryBot.create(:post, user_id: user.id) }
+  let(:user) { FactoryBot.create(:user) }
+  let(:comment_post) { FactoryBot.create(:post, user_id: user.id) }
 
   before do
     sign_in user
   end
 
   describe 'POST comments#create' do
-    context 'with content' do
-      let(:params) do
-        { post_id: comment_post.id, content: 'akhsdgjhsd' }
-      end
-
+    context 'with correct params' do
       it 'creates a new comment' do
         expect do
-          post :create, params: params, xhr: true
+          post :create, params: { post_id: comment_post.id, content: 'This is a dummy content' }, xhr: true
         end.to change(Comment, :count).by(+1)
         expect(response).to have_http_status(:ok)
       end
     end
 
-    context 'without content' do
-      let(:params) do
-        { post_id: comment_post.id, content: nil }
-      end
-
-      it 'will not creates a new comment' do
+    context 'with wrong params' do
+      it 'empty content will not create a new comment' do
         expect do
-          post :create, params: params, xhr: true
+          post :create, params: { post_id: comment_post.id, content: nil }, xhr: true
         end.to change(Comment, :count).by(0)
         expect(flash[:alert]).to include("Content can't be blank")
         expect(response).to have_http_status(:ok)
       end
+
+      it 'wrong post id will not create a new comment' do
+        expect do
+          post :create, params: { post_id: 0, content: 'This is a dummy content' }, xhr: true
+        end.to change(Comment, :count).by(0)
+        expect(flash[:alert]).to eq('Record Not Found')
+        expect(response).to redirect_to root_path
+      end
     end
+
+    context 'when user is not authenticate' do
+      it 'will make user to sign in' do
+        sign_out user
+        expect do
+          post :create, params: { post_id: comment_post.id, content: 'This is a dummy content' }, xhr: true
+        end.to change(Comment, :count).by(0)
+        expect(response).to have_http_status(:unauthorized)
+        expect(response.body).to eq('You need to sign in or sign up before continuing.')
+      end
+    end
+
+    # context 'when user is not authorize' do
+    #   it 'will not allow user to perform action' do
+    #     expect do
+    #       post :create, params: { post_id: comment_post.id, content: 'This is a dummy content' }, xhr: true
+    #     end.to change(Comment, :count).by(1)
+    #     expect(response).to have_http_status(:ok)
+    #   end
+    # end
   end
 
   describe 'GET comments#edit' do
@@ -53,9 +73,21 @@ RSpec.describe CommentsController, type: :controller do
 
     context 'without created comment' do
       it 'will not renders edit template' do
-        get :edit, params: { id: 339_816 }
+        get :edit, params: { id: 0 }
         expect(flash[:alert]).to include('Record Not Found')
         expect(response).to have_http_status(:found)
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context 'when user is not authenticate' do
+      it 'will make user to sign in' do
+        sign_out user
+        expect do
+          post :create, params: { post_id: comment_post.id, content: 'This is a dummy content' }, xhr: true
+        end.to change(Comment, :count).by(0)
+        expect(response).to have_http_status(:unauthorized)
+        expect(response.body).to eq('You need to sign in or sign up before continuing.')
       end
     end
   end
@@ -65,7 +97,7 @@ RSpec.describe CommentsController, type: :controller do
       let(:my_new_comment) { FactoryBot.create(:comment, user_id: user.id) }
 
       it 'updates the comment with content' do
-        put :update, params: { id: my_new_comment.id, comment: { content: 'kskajbs' } }
+        put :update, params: { id: my_new_comment.id, comment: { content: 'This is a dummy content' } }
         expect(flash[:notice]).to include('Comment updated successfully.')
         expect(response).to redirect_to post_path(my_new_comment.post)
       end
@@ -82,6 +114,18 @@ RSpec.describe CommentsController, type: :controller do
         put :update, params: { id: 339_816, comment: { content: '' } }
         expect(flash[:alert]).to include('Record Not Found')
         expect(response).to have_http_status(:found)
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context 'when user is not authenticate' do
+      it 'will make user to sign in' do
+        sign_out user
+        expect do
+          post :create, params: { post_id: comment_post.id, content: 'This is a dummy content' }, xhr: true
+        end.to change(Comment, :count).by(0)
+        expect(response).to have_http_status(:unauthorized)
+        expect(response.body).to eq('You need to sign in or sign up before continuing.')
       end
     end
   end
@@ -101,6 +145,18 @@ RSpec.describe CommentsController, type: :controller do
         delete :destroy, params: { id: 339_816 }, xhr: true
         expect(flash[:alert]).to include('Record Not Found')
         expect(response).to have_http_status(:ok)
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context 'when user is not authenticate' do
+      it 'will make user to sign in' do
+        sign_out user
+        expect do
+          post :create, params: { post_id: comment_post.id, content: 'This is a dummy content' }, xhr: true
+        end.to change(Comment, :count).by(0)
+        expect(response).to have_http_status(:unauthorized)
+        expect(response.body).to eq('You need to sign in or sign up before continuing.')
       end
     end
   end
